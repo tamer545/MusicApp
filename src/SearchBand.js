a47493a1a0a9a6f8d2d75282646809c351506167
Tamer545 <8235.Lohn>
2021-11-07 18:44:00 +0100

    Music App

--- src/SearchBand.js
+++ src/SearchBand.js
@@ -29,41 +29,40 @@ export default function SearchBand({searchQuery}) {
 
     function Performances() {
         return (
-                <Container>
-                    <Accordion>
-                        {band.performances.map((bandPerformance) => {
-                            if (bandPerformance.location) {
-                                return (
-                                    <Card>
-                                        {}
-                                        <Accordion.Toggle as={Card.Header} eventKey="0">
-                                            {bandPerformance.location}
-                                        </Accordion.Toggle>
-                                        <Accordion.Collapse eventKey="0">
-                                            <Card.Body>
-                                                <table>
-                                                    <thead>
-                                                    <tr>
-                                                        <th scope="col">Date</th>
-                                                        <th scope="col">URL</th>
-                                                    </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                    <tr>
-                                                        <td style={{paddingRight: '100px'}}>{bandPerformance.date.substring(0, 10) || "unknown"}</td>
-                                                        <td>{<a
-                                                            href={bandPerformance.location_url}>{bandPerformance.location_url ? "Homepage" : "unknown"}</a> || "unknown"}</td>
-                                                    </tr>
-                                                    </tbody>
-                                                </table>
-                                            </Card.Body>
-                                        </Accordion.Collapse>
-                                    </Card>
-                                )
-                            }
-                        })}
-                    </Accordion>
-                </Container>
+            <Container>
+                <Accordion>
+                    {band.performances.map((bandPerformance) => {
+                        if (bandPerformance.location) {
+                            return (
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="0">
+                                        {bandPerformance.location}
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="0">
+                                        <Card.Body>
+                                            <table>
+                                                <thead>
+                                                <tr>
+                                                    <th scope="col">Date</th>
+                                                    <th scope="col">URL</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr>
+                                                    <td style={{paddingRight: '100px'}}>{bandPerformance.date.substring(0, 10) || "unknown"}</td>
+                                                    <td>{<a
+                                                        href={bandPerformance.location_url}>{bandPerformance.location_url ? "Homepage" : "unknown"}</a> || "unknown"}</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                            )
+                        }
+                    })}
+                </Accordion>
+            </Container>
         )
     }
 
@@ -80,32 +79,32 @@ export default function SearchBand({searchQuery}) {
         )
     } else if (band !== undefined) {
         return (
-                <Card style={{width: '100%'}}>
-                    <Card.Img style={{width: '10%'}} variant="roundedCircle" src={band?.image} alt={"Band Icon"}/>
-                    <Card.Body>
-                        <Card.Title><h3>{band?.name}</h3></Card.Title>
-                        <br/>
-                        <Button href={band?.permalink}>Go To Band</Button>
-                        <br/>
-                        <br/>
-                        <Card.Text>
-                            <h5>City</h5>
-                            {band?.city === null ? "unknown" : band?.city}<br/>
-                        </Card.Text>
-                        <Card.Text>
-                            <h5>Email</h5>
-                            {band?.email === null ? "unknown" : band?.email}
-                        </Card.Text>
-                        <Card.Text>
-                            <h5>Biography</h5>
-                            {band?.biographies[0]?.description || "no biography"}
-                        </Card.Text>
-                        <Card.Text>
-                            <h5>Performances</h5>
-                            <Performances/>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+            <Card style={{width: '100%'}}>
+                <Card.Img style={{width: '10%'}} variant="roundedCircle" src={band?.image} alt={"Band Icon"}/>
+                <Card.Body>
+                    <Card.Title><h3>{band?.name}</h3></Card.Title>
+                    <br/>
+                    <Button href={band?.permalink}>Go To Band</Button>
+                    <br/>
+                    <br/>
+                    <Card.Text>
+                        <h5>City</h5>
+                        {band?.city === null ? "unknown" : band?.city}<br/>
+                    </Card.Text>
+                    <Card.Text>
+                        <h5>Email</h5>
+                        {band?.email === null ? "unknown" : band?.email}
+                    </Card.Text>
+                    <Card.Text>
+                        <h5>Biography</h5>
+                        {band?.biographies[0]?.description || "no biography"}
+                    </Card.Text>
+                    <Card.Text>
+                        <h5>Performances</h5>
+                        <Performances/>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         );
     }
 }
